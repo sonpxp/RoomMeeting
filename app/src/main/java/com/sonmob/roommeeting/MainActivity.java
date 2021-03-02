bffa205675e427b228878f66a03dcf4f932f4a18
sonpxp <sonmob202@gmail.com>
2021-03-02 14:58:35 +0700

    Design and import library

---
+++ app/src/main/java/com/sonmob/roommeeting/MainActivity.java
@@ -0,0 +1,35 @@
+package com.sonmob.roommeeting;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Window window = this.getWindow();
+        // clear FLAG_TRANSLUCENT_STATUS flag:
+        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        // add FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS flag to the window
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+        // finally change the color
+        window.setStatusBarColor(ContextCompat.getColor(this, R.color.white));
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                    startActivity(new Intent(MainActivity.this, LoginActivity.class));
+                finish();
+            }
+        }, 2000);
+    }
+}
